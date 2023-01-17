cd35aeebbb471bcaa15362e3eb5228b7f273ae10
Kiran K <kiran22292@gmail.com>
2023-01-17 13:03:39 +0000

    Final commit

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const TIMEOUT = 1500;
 
+const renderMarker = (map, data) => {
+  const marker = L.marker(data.coords)
+  markerHelper(marker, map, data)
+  return marker;
+};
+
+
+function markerHelper(marker, map, data) {
+  marker.bindTooltip(`<b>${data.country} : ${data.year} FIFA World Cup Winners</b>`,
+    {
+      permanent: true,
+      direction: 'right'
+    });
+  map.addLayer(marker)
+  map.setView(data.coords, 4)
+}
+
 const renderMap = () => {
   const div = document.createElement('div');
   div.setAttribute('id', 'map')
   document.body.appendChild(div)
 
   const map = L.map('map', {
-    minZoom: 4
+    minZoom: 2
   }).setView([0, 0], 0);
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -14,35 +31,24 @@ const renderMap = () => {
   }).addTo(map);
 
   setTimeout(() => {
-    map.invalidateSize()
+    map.invalidateSize();
   })
 
   return map;
 };
 
-const makePromise = (data, index, map) => new Promise((resolve) => {
+const makePromise = (data, index, marker, map) => new Promise((resolve) => {
   setTimeout(() => {
-    const marker = L.marker(data.coords)
-    marker.bindTooltip(`<b>${data.country} : ${data.year} FIFA World Cup Winners</b>`,
-      {
-        permanent: true,
-        direction: 'right'
-      });
-    map.addLayer(marker)
-    map.setView(data.coords, 4)
+    marker.setLatLng(data.coords);
+    markerHelper(marker, map, data)
     resolve({ marker });
   }, TIMEOUT * index);
 });
 
-const sequenceWinners = (map) => {
-  const sequencePromises = WINNING_NATIONS_COORDS
-    .map((data, index) => makePromise(data, index, map))
-    .reduce((acc, curr) => acc.then(() => curr)
-      .then(({ marker }) => {
-        setTimeout(() => {
-          map.removeLayer(marker)
-        }, TIMEOUT)
-      }), Promise.resolve())
+const sequenceWinners = (map, marker) => {
+  const sequencePromises = WINNING_NATIONS_COORDS.slice(1)
+    .map((data, index) => makePromise(data, index, marker, map))
+    .reduce((acc, curr) => acc.then(() => curr), Promise.resolve())
 
   sequencePromises.then(() => {
     setTimeout(() => {
@@ -51,7 +57,7 @@ const sequenceWinners = (map) => {
         autoClose: false
       })
         .setLatLng([0, 0])
-        .setContent('<b>You have run through all fifa winners</b>')
+        .setContent('<b>You have run through all FIFA winners</b>')
         .openOn(map);
       map.setView([0, 0], 0)
     }, TIMEOUT);
@@ -62,5 +68,8 @@ const sequenceWinners = (map) => {
 
 document.addEventListener('DOMContentLoaded', () => {
   const map = renderMap();
-  sequenceWinners(map);
+  const marker = renderMarker(map, WINNING_NATIONS_COORDS[0]);
+  setTimeout(() => {
+    sequenceWinners(map, marker);
+  }, TIMEOUT);
 })
